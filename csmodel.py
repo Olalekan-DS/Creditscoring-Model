a3e00add21ba804115b489712156135cc4740443
Olalekan-DS <83697621+Olalekan-DS@users.noreply.github.com>
2021-08-02 23:59:42 +0100

    Update csmodel.py

--- csmodel.py
+++ csmodel.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 pd.pandas.set_option('display.max_columns', None)
+import seaborn as sns #Graph library that use matplot in background
 # for plotting
 import matplotlib.pyplot as plt
 #%matplotlib inline
@@ -24,6 +25,14 @@ from sklearn.feature_selection import f_classif
 from scipy.stats import chi2_contingency
 from math import sqrt
 
+#plotly libary
+import plotly.offline as py 
+py.init_notebook_mode(connected=True) # this code, allow us to work with offline plotly version
+import plotly.graph_objs as go # it's like "plt" of matplot
+import plotly.tools as tls # It's useful to we get some tools of plotly
+import warnings # This library will be used to ignore some warnings
+from collections import Counter # To do counter of some features
+
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -31,9 +40,129 @@ warnings.filterwarnings('ignore')
 from sklearn.preprocessing import RobustScaler
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
-
+#############################################################################################################
 df = pd.read_csv('/Users/user/Documents/creditscore_new.csv')
 df.shape
+print(df.info())
+df.isnull().sum()
+
+#EDA
+#Let's start looking through target variable and their distribuition
+trace0 = go.Bar(
+            x = df[df["Risk_new"]== 'good']["Risk_new"].value_counts().index.values,
+            y = df[df["Risk_new"]== 'good']["Risk_new"].value_counts().values,
+            name='Good credit'
+    )
+trace1 = go.Bar(
+            x = df[df["Risk_new"]== 'medium']["Risk_new"].value_counts().index.values,
+            y = df[df["Risk_new"]== 'medium']["Risk_new"].value_counts().values,
+            name='Medium credit'
+    )
+trace2 = go.Bar(
+            x = df[df["Risk_new"]== 'bad']["Risk_new"].value_counts().index.values,
+            y = df[df["Risk_new"]== 'bad']["Risk_new"].value_counts().values,
+            name='Bad credit'
+    )
+
+data = [trace0, trace1, trace2]
+
+layout = go.Layout()
+
+layout = go.Layout(
+    yaxis=dict(
+        title='Count'
+    ),
+    xaxis=dict(
+        title='Risk Variable'
+    ),
+    title='Target variable distribution'
+)
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig, filename='grouped-bar')
+
+#Looking at the distribuition of loan status by Risk level
+#First plot
+trace0 = go.Bar(
+    x = df[df["Risk_new"]== 'good']["loan_status"].value_counts().index.values,
+    y = df[df["Risk_new"]== 'good']["loan_status"].value_counts().values,
+    name='Good credit'
+)
+#Second plot
+trace1 = go.Bar(
+    x = df[df["Risk_new"]== 'medium']["loan_status"].value_counts().index.values,
+    y = df[df["Risk_new"]== 'medium']["loan_status"].value_counts().values,
+    name='Medium credit'
+)
+#third plot
+trace2 = go.Bar(
+    x = df[df["Risk_new"]== 'bad']["loan_status"].value_counts().index.values,
+    y = df[df["Risk_new"]== 'bad']["loan_status"].value_counts().values,
+    name="Bad Credit"
+)
+
+data = [trace0, trace1, trace2]
+
+layout = go.Layout(
+    title='Loan Status Distribuition'
+)
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig, filename='Loan_Status-Grouped')
+
+#Data Distribution by partner
+df_good = df.loc[df["Risk_new"] == 'good']['partner_name'].values.tolist()
+df_medium = df.loc[df["Risk_new"] == 'medium']['partner_name'].values.tolist()
+df_bad = df.loc[df["Risk_new"] == 'bad']['partner_name'].values.tolist()
+df_partner = df['partner_name'].values.tolist()
+
+#First plot
+trace0 = go.Histogram(
+    x=df_good,
+    histnorm='probability',
+    name="Good Credit"
+)
+#Second plot
+trace1 = go.Histogram(
+    x=df_medium,
+    histnorm='probability',
+    name="Medium Credit"
+)
+#Third plot
+trace2 = go.Histogram(
+    x=df_bad,
+    histnorm='probability',
+    name="Bad Credit"
+)
+#Fourth plot
+trace3 = go.Histogram(
+    x=df_partner,
+    histnorm='probability',
+    name="Partner"
+)
+
+data = [trace0, trace1, trace2]
+
+layout = go.Layout(
+    title='General Distribuition'
+)
+
+fig = go.Figure(data=data, layout=layout)
+fig['layout'].update(showlegend=True, title='Partner Distribuition', bargap=0.05)
+py.iplot(fig, filename='custom-sized-subplot-with-subplot-titles')
+
+#Cross tabulating anount to pay with partner
+fig, ax = plt.subplots(figsize=(20,20), nrows=2)
+g1 = sns.boxplot(x="partner_name", y="balance_remaining", data=df, palette="hls", ax=ax[0], hue="Risk_new")
+g1.set_title("Balance Remaining by Partner", fontsize=12)
+g1.set_xlabel("Partner", fontsize=12)
+g1.set_ylabel("Credit Amount", fontsize=12)
+
+g2 = sns.violinplot(x="call_contact_status", y="balance_remaining", data=df, ax=ax[1], hue="Risk_new", palette="hls")
+g2.set_title("Credit Amount by Call", fontsize=12)
+g2.set_xlabel("Call", fontsize=12)
+g2.set_ylabel("Credit Amount", fontsize=12)
+
+plt.subplots_adjust(hspace = 0.4,top = 0.9)
+plt.show()
 
 # Select independent and dependent variable
 include = ['partner_name', 'days_late',
